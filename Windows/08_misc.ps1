4ebd71d1c442e73c7667acef158c85ce7c6afb02
Marc-André Bär <mabaer@users.noreply.github.com>
2018-02-19 20:46:17 +0100

    Added Misc Module

--- Windows/08_misc.ps1
+++ Windows/08_misc.ps1
@@ -1,4 +1,12 @@
 
+# 08_01 Screensaver Settings
+Get-Wmiobject win32_desktop | Export-Csv -Path $PSScriptRoot"\"$outputDir"\08_01-"$outputDir"-ScreenSaver.csv"
+
+# 08_02 System Resume
+Get-ADDefaultDomainPasswordPolicy | Format-List lockout* | Export-Csv -Path $PSScriptRoot"\"$outputDir"\08_02-"$outputDir"-System_Lockout.csv"
+
+# 08_03 PATH Elements
+$Env:Path | Export-Csv -Path $PSScriptRoot"\"$outputDir"\08_03-"$outputDir"-PATH.csv"
 
 # Wait for user input and close the script>
 Read-Host -Prompt "Execution completed succesfully. Press Enter to exit"
