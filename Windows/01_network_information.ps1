e62f391ac6bc1a290d61d75139eb9d04d59d853d
Marc-André Bär <mabaer@users.noreply.github.com>
2018-02-20 18:58:40 +0100

    Update Network Info

--- Windows/01_network_information.ps1
+++ Windows/01_network_information.ps1
@@ -9,3 +9,7 @@ Get-SmbShare | Get-SmbShareAccess | Export-Csv -Path $PSScriptRoot"\"$outputDir"
 Get-NetTCPConnection -State established | Export-Csv -Path $PSScriptRoot"\"$outputDir"\01_03-"$outputDir"-Established_Connections.csv"
 
 # 01_04 Network Services
+
+# 01_05 TCP and UDP Settings
+Get-NetTCPSetting | Export-Csv -Path $PSScriptRoot"\"$outputDir"\01_05-"$outputDir"-TCP_Settings.csv"
+Get-NetUDPSetting | Export-Csv -Path $PSScriptRoot"\"$outputDir"\01_05-"$outputDir"-UDP_Settings.csv"
