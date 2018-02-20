191bd0acce8e16ebf015b5e4f79e4c659ad9427b
Marc-André Bär <mabaer@users.noreply.github.com>
2018-02-20 18:59:44 +0100

    Update Misc

--- Windows/08_misc.ps1
+++ Windows/08_misc.ps1
@@ -8,5 +8,14 @@ Get-ADDefaultDomainPasswordPolicy | Format-List lockout* | Export-Csv -Path $PSS
 # 08_03 PATH Elements
 $Env:Path | Export-Csv -Path $PSScriptRoot"\"$outputDir"\08_03-"$outputDir"-PATH.csv"
 
+# 08_04 Secure Boot
+Get-SecureBootPolicy | Export-Csv -Path $PSScriptRoot"\"$outputDir"\08_04-"$outputDir"-Secure_Boot_Policy.csv"
+
+# 08_05 Connected Devices
+Get-PnpDevice –PresentOnly | Export-Csv -Path $PSScriptRoot"\"$outputDir"\08_05-"$outputDir"-Connected_Devices.csv"
+
+# 08_06 Expiring Certificates
+Get-ChildItem Cert:\LocalMachine\My\ -ExpiringInDays 60 | Select-Object Thumbprint, Subject, NotAfter | Export-Csv -Path $PSScriptRoot"\"$outputDir"\08_06-"$outputDir"-Expiring_Certificates.csv"
+
 # Wait for user input and close the script>
 Read-Host -Prompt "Execution completed succesfully. Press Enter to exit"
