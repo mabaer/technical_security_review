57b443d03639b43c243b09ec118c928ea604a573
Marc-André Bär <mabaer@users.noreply.github.com>
2018-02-20 18:58:52 +0100

    Update System Info

--- Windows/02_system_information.ps1
+++ Windows/02_system_information.ps1
@@ -6,9 +6,13 @@ Get-WmiObject -Class Win32_ComputerSystem | Export-Csv -Path $PSScriptRoot"\"$ou
 # 02_02 Domain Information
 $env:UserDomain | Export-Csv -Path $PSScriptRoot"\"$outputDir"\02_02-"$outputDir"-Domain_Name.csv"
 $env:userdnsdomain | Export-Csv -Path $PSScriptRoot"\"$outputDir"\02_02-"$outputDir"-Domain_DNS_Name.csv"
+Get-ComputerInfo | Export-Csv -Path $PSScriptRoot"\"$outputDir"\02_01-"$outputDir"-Computer_Info.csv"
 
 # 02_03 Admin Account
 Get-LocalGroupMember -Group "Administrators" | Export-Csv -Path $PSScriptRoot"\"$outputDir"\02_01-"$outputDir"-Administrators.csv"
 
 # 02_04 Guest Account
 Get-WmiObject -Class Win32_UserAccount -Filter "LocalAccount='$true'" | Select-Object Name,Disabled | FT –AutoSize | Export-Csv -Path $PSScriptRoot"\"$outputDir"\02_04-"$outputDir"-Disabled_Users.csv"
+
+# 02_05 Storage Information
+Get-PSDrive | Export-Csv -Path $PSScriptRoot"\"$outputDir"\02_05-"$outputDir"-Storage_Info.csv"
