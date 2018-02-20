73c14239f7ca6df0258552e3f2e6fea681f36275
Marc-André Bär <mabaer@users.noreply.github.com>
2018-02-20 18:59:23 +0100

    Update Installed Software

--- Windows/05_installed_software.ps1
+++ Windows/05_installed_software.ps1
@@ -4,3 +4,6 @@ Get-WmiObject -Class "win32_quickfixengineering" | Select-Object -Property "Desc
 
 # 05_02 Installed Applications
 Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |  Select-Object DisplayName, DisplayVersion, Publisher, InstallDate |  FT –AutoSize | Export-Csv -Path $PSScriptRoot"\"$outputDir"\05_02-"$outputDir"-Installed_Applications.csv"
+
+# 05_03 Security Patches within last 90 Days
+Get-CimInstance -Class win32_quickfixengineering | Where-Object { $_.InstalledOn -gt (Get-Date).AddMonths(-3) } | Export-Csv -Path $PSScriptRoot"\"$outputDir"\05_03-"$outputDir"-Patches_Last_90d.csv"
