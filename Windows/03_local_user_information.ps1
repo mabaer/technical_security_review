bed027a204b2c5f1c242c5c45251e501bf1866db
Marc-André Bär <mabaer@users.noreply.github.com>
2018-02-25 11:08:10 +0100

    Added Local Groups and User Priv Functions

--- Windows/03_local_user_information.ps1
+++ Windows/03_local_user_information.ps1
@@ -1,7 +1,21 @@
 
 # 03_01 Local Users
-Get-WmiObject -Class Win32_UserAccount -Filter  "LocalAccount='True'" | Select PSComputername, Name, Domain, Caption, Status, Disabled, AccountType, Lockout, PasswordRequired, PasswordChangeable, SID | FT –AutoSize | Export-Csv -Path $PSScriptRoot"\"$outputDir"\03_01-"$outputDir"-Local_User.csv"
+Get-WmiObject -Class Win32_UserAccount -Filter  "LocalAccount='True'" | Select * | FT –AutoSize | Export-Csv -Path $PSScriptRoot"\"$outputDir"\03_01-"$outputDir"-Local_User.csv"
 
 # 03_02 Local Groups
+Get-WMIObject win32_group -filter "LocalAccount='True'" | Select * | Export-Csv -Path $PSScriptRoot"\"$outputDir"\03_02-"$outputDir"-Local_Groups.csv"
+$Groups = Get-WMIObject win32_group -filter "LocalAccount='True'" 
+$MemberArray = New-Object -typeName System.Collections.Arraylist
+Foreach($G In $Groups)
+{
+    $Members = Get-LocalGroupMember -Group $G.Name
+    $SaveString = "$($Members)"
+    $MemberArray += New-Object PsObject -property @{
+        'Group' = $G.Name
+        'Members' = $SaveString
+    }
+}
+$MemberArray | Export-Csv -Path $PSScriptRoot"\"$outputDir"\03_02-"$outputDir"-Local_Groups_Members.csv" -NoTypeInformation
 
 # 03_03 Assigned Administrative Privileges
+secedit /export /areas USER_RIGHTS /cfg $PSScriptRoot"\"$outputDir"\03_03-"$outputDir"-Assigned_Privileges.csv"
